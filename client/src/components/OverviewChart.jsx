7b61c052ef19d7e45376c9f4a54fedbe074c2493
greatshah222 <great.shah222@gmail.com>
2023-01-08 02:20:23 +0200

    dashboard added

--- client/src/components/OverviewChart.jsx
+++ client/src/components/OverviewChart.jsx
@@ -3,7 +3,7 @@ import { useTheme } from "@mui/material";
 import { useGetSalesQuery } from "../state/api";
 import { useMemo } from "react";
 import { Box } from "@mui/system";
-import Header from "./Header";
+
 const OverviewChart = ({ view, isDashboard = false }) => {
   const theme = useTheme();
 
@@ -63,121 +63,121 @@ const OverviewChart = ({ view, isDashboard = false }) => {
 
   if (!data || isLoading) return "Loading...";
   return (
-    <Box height={"75vh"}>
-      <ResponsiveLine
-        data={view === "sales" ? totalSalesLine : totalUnitsLine}
-        theme={{
-          axis: {
-            domain: {
-              line: {
-                stroke: theme.palette.secondary[200],
-              },
-            },
-            legend: {
-              text: {
-                fill: theme.palette.secondary[200],
-              },
-            },
-            ticks: {
-              line: {
-                stroke: theme.palette.secondary[200],
-                strokeWidth: 1,
-              },
-              text: {
-                fill: theme.palette.secondary[200],
-              },
+    <ResponsiveLine
+      data={view === "sales" ? totalSalesLine : totalUnitsLine}
+      theme={{
+        axis: {
+          domain: {
+            line: {
+              stroke: theme.palette.secondary[200],
             },
           },
-          legends: {
+          legend: {
             text: {
               fill: theme.palette.secondary[200],
             },
           },
-          tooltip: {
-            container: {
-              color: theme.palette.primary.main,
+          ticks: {
+            line: {
+              stroke: theme.palette.secondary[200],
+              strokeWidth: 1,
+            },
+            text: {
+              fill: theme.palette.secondary[200],
             },
           },
-        }}
-        margin={{ top: 20, right: 50, bottom: 50, left: 70 }}
-        xScale={{ type: "point" }}
-        yScale={{
-          type: "linear",
-          min: "auto",
-          max: "auto",
-          stacked: false,
-          reverse: false,
-        }}
-        // for more curvy line
-        curve="catmullRom"
-        yFormat=" >-.2f"
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-          format: (v) => {
-            // just slicing the letter in dashboard cause of less space
-            if (isDashboard) return v.slice(0, 3);
-            return v;
+        },
+        legends: {
+          text: {
+            fill: theme.palette.secondary[200],
           },
-          orient: "bottom",
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: isDashboard ? "" : "Month",
-          legendOffset: 36,
-          legendPosition: "middle",
-        }}
-        axisLeft={{
-          orient: "left",
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: isDashboard
-            ? ""
-            : `Total ${view === "sales" ? "Revenue" : "Units"} for year`,
-          legendOffset: -60,
-          legendPosition: "middle",
-        }}
-        enableGridX={false}
-        enableGridY={false}
-        pointSize={10}
-        pointColor={{ theme: "background" }}
-        pointBorderWidth={2}
-        pointBorderColor={{ from: "serieColor" }}
-        pointLabelYOffset={-12}
-        useMesh={true}
-        legends={
-          isDashboard
-            ? undefined
-            : [
-                {
-                  anchor: "bottom-right",
-                  direction: "column",
-                  justify: false,
-                  translateX: 30,
-                  translateY: -40,
-                  itemsSpacing: 0,
-                  itemDirection: "left-to-right",
-                  itemWidth: 80,
-                  itemHeight: 20,
-                  itemOpacity: 0.75,
-                  symbolSize: 12,
-                  symbolShape: "circle",
-                  symbolBorderColor: "rgba(0, 0, 0, .5)",
-                  effects: [
-                    {
-                      on: "hover",
-                      style: {
-                        itemBackground: "rgba(0, 0, 0, .03)",
-                        itemOpacity: 1,
-                      },
+        },
+        tooltip: {
+          container: {
+            color: theme.palette.primary.main,
+          },
+        },
+      }}
+      margin={{ top: 20, right: 50, bottom: 50, left: 70 }}
+      xScale={{ type: "point" }}
+      yScale={{
+        type: "linear",
+        min: "auto",
+        max: "auto",
+        stacked: false,
+        reverse: false,
+      }}
+      // for more curvy line
+      curve="catmullRom"
+      enableArea={isDashboard}
+      yFormat=" >-.2f"
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        format: (v) => {
+          // just slicing the letter in dashboard cause of less space
+          if (isDashboard) return v.slice(0, 3);
+          return v;
+        },
+        orient: "bottom",
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: isDashboard ? "" : "Month",
+        legendOffset: 36,
+        legendPosition: "middle",
+      }}
+      axisLeft={{
+        orient: "left",
+        tickValues: 5,
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: isDashboard
+          ? ""
+          : `Total ${view === "sales" ? "Revenue" : "Units"} for year`,
+        legendOffset: -60,
+        legendPosition: "middle",
+      }}
+      enableGridX={false}
+      enableGridY={false}
+      pointSize={10}
+      pointColor={{ theme: "background" }}
+      pointBorderWidth={2}
+      pointBorderColor={{ from: "serieColor" }}
+      pointLabelYOffset={-12}
+      useMesh={true}
+      legends={
+        isDashboard
+          ? undefined
+          : [
+              {
+                anchor: "bottom-right",
+                direction: "column",
+                justify: false,
+                translateX: 30,
+                translateY: -40,
+                itemsSpacing: 0,
+                itemDirection: "left-to-right",
+                itemWidth: 80,
+                itemHeight: 20,
+                itemOpacity: 0.75,
+                symbolSize: 12,
+                symbolShape: "circle",
+                symbolBorderColor: "rgba(0, 0, 0, .5)",
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemBackground: "rgba(0, 0, 0, .03)",
+                      itemOpacity: 1,
                     },
-                  ],
-                },
-              ]
-        }
-      />
-    </Box>
+                  },
+                ],
+              },
+            ]
+      }
+    />
   );
 };
 
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,236 @@
+import React from "react";
+
+import {
+  DownloadOutlined,
+  Email,
+  PointOfSale,
+  PersonAdd,
+  Traffic,
+} from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { useGetDashboardQuery } from "../state/api";
+import FlexBetween from "../components/FlexBetweens";
+import Header from "../components/Header";
+import { fontWeight } from "@mui/system";
+import Statbox from "../components/Statbox";
+import OverviewChart from "../components/OverviewChart";
+import BreakDownChart from "../components/BreakDownChart";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const theme = useTheme();
+  const isNonMediumScreens = useMediaQuery("(min-width:1200px)");
+
+  const { data, isLoading } = useGetDashboardQuery();
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "userId",
+      headerName: "User ID",
+      flex: 1,
+    },
+    {
+      field: "createdAt",
+      headerName: "createdAt",
+      flex: 1,
+    },
+
+    {
+      field: "products",
+      headerName: "# of Products",
+      flex: 0.5,
+      sortable: false,
+      renderCell: (params) => params.value.length,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => `€${Number(params.value).toFixed(2)}`,
+    },
+  ];
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      {/* HEader */}
+      <FlexBetween>
+        <Header title={"DASHBOARD"} subTitle="Welcome to your dashboard " />
+
+        <Box>
+          <Button
+            sx={{
+              backgroundColor: theme.palette.primary[400],
+              color: theme.palette.secondary[400],
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+            <DownloadOutlined sx={{ mr: "10px" }} />
+            Download Reports
+          </Button>
+        </Box>
+      </FlexBetween>
+
+      <Box
+        display={"grid"}
+        gridTemplateColumns="repeat(12,1fr)"
+        gridAutoRows={"160px"}
+        gap="20px"
+        sx={{
+          "& >div": {
+            gridColumn: isNonMediumScreens ? undefined : "span 12",
+          },
+        }}
+      >
+        {/* ROW 1 */}
+
+        <Statbox
+          title={"Total Customer"}
+          value={(data && data.totalCustomers) || 18789}
+          increase={"+14%"}
+          description="Since last month"
+          icon={
+            <Email
+              sx={{
+                fontSize: "26px",
+                color: theme.palette.secondary[300],
+              }}
+            />
+          }
+        />
+        <Statbox
+          title={"Sales today"}
+          value={data && data.todayStats.totalSales}
+          increase={"+24%"}
+          description="Since last month"
+          icon={
+            <PointOfSale
+              sx={{
+                fontSize: "26px",
+                color: theme.palette.secondary[300],
+              }}
+            />
+          }
+        />
+        <Box
+          gridColumn="span 8"
+          gridRow="span 2"
+          backgroundColor={theme.palette.background.alt}
+          p="1rem"
+          borderRadius={"0.55rem"}
+        >
+          <OverviewChart isDashboard={true} view="sales" />
+        </Box>
+        <Statbox
+          title={"Monthly Sales"}
+          value={data && data.thisMonthStats.totalSales}
+          increase={"+5%"}
+          description="Since last month"
+          icon={
+            <PersonAdd
+              sx={{
+                fontSize: "26px",
+                color: theme.palette.secondary[300],
+              }}
+            />
+          }
+        />
+        <Statbox
+          title={"Yearly Sales"}
+          value={data && data.yearlySalesTotal}
+          increase={"+44%"}
+          description="Since last month"
+          icon={
+            <Traffic
+              sx={{
+                fontSize: "26px",
+                color: theme.palette.secondary[300],
+              }}
+            />
+          }
+        />
+
+        {/* ROW 2 */}
+        {/* LEFT */}
+        <Box
+          gridColumn={"span 8"}
+          gridRow="span 3"
+          sx={{
+            "& .MuiDataGrid-root": {
+              border: "none",
+              borderRadius: "5rem",
+            },
+            "& .MuiDataGrid-cell": {
+              borderBottom: "none",
+            },
+            "& .MuiDataGrid-columnHeaders": {
+              backgroundColor: theme.palette.background.alt,
+              color: theme.palette.secondary[100],
+              borderBottom: "none",
+            },
+            "& .MuiDataGrid-virtualScroller": {
+              backgroundColor: theme.palette.background.alt,
+            },
+            "& .MuiDataGrid-footerContainer": {
+              backgroundColor: theme.palette.background.alt,
+              color: theme.palette.secondary[100],
+              borderTop: "none",
+            },
+            "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+              color: `${theme.palette.secondary[200]} !important`,
+            },
+          }}
+        >
+          <DataGrid
+            rows={(data && data.transactions) || []}
+            columns={columns}
+            loading={isLoading || !data}
+            // by default it looks for .id so we are changing it to look for ._id
+            getRowId={(row) => row._id}
+          />
+        </Box>
+        <Box
+          gridColumn={"span 4"}
+          gridRow="span 3"
+          backgroundColor={theme.palette.background.alt}
+          p="1.5rem"
+          borderRadius={"0.55rem"}
+        >
+          <Typography
+            variant="h6"
+            sx={{
+              color: theme.palette.secondary[100],
+            }}
+          >
+            Sales By Category
+          </Typography>
+          <BreakDownChart isDashboard={true} />
+          <Typography
+            variant="h6"
+            p="0 0.6rem"
+            fontSize={"0.8rem"}
+            sx={{
+              color: theme.palette.secondary[200],
+            }}
+          >
+            Breakdown of sales and information
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Dashboard;
